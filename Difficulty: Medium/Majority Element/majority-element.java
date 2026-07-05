06d15d44c1060daa8adb65ef24dc067b08ca1b62
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-07-05 13:28:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    int majorityElement(int nums[]) {
+        // code here
+        int count = 1;
+        int el = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                el = nums[i];
+                count = 1;
+            } else if (nums[i] == el) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int num : nums) {
+            if (num == el) {
+                count++;
+            }
+        }
+        if (count > nums.length / 2) return el;
+        return -1;
+    }
+}
