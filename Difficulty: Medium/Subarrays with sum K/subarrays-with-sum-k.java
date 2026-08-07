ec275cd4e2204f85fa2516e4ef90faa4495d62a7
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-08-07 13:58:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int cntSubarrays(int[] nums, int k) {
+        // code here
+        int cnt = 0;
+        int sum = 0;
+        Map<Integer,Integer>map = new HashMap<>();
+        map.put(0,1);
+        for (int i : nums) {
+            sum += i;
+            if (map.containsKey(sum - k)) {
+                cnt += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return cnt;
+    }
+}
