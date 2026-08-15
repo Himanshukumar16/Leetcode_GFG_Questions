1b97b6700e54a1a124ea93b66f348af1ff5b1a89
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-08-15 15:52:59 +0530

    Time: 182 ms (5.4%), Space: 122.8 MB (6.65%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPrimes(int n) {
+        ArrayList<Integer> al = new ArrayList<>();
+        int[] isPrimeArr = new int[n + 1];
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeArr[i] == 0) {
+                for (int j = 2 * i; j < n; j += i) {
+                    isPrimeArr[j] = 1;
+                }
+            }
+        }
+        for (int i = 2; i < isPrimeArr.length-1; i++) {
+            if (isPrimeArr[i] == 0) al.add(i);
+        }
+        return al.size();
+    }
+}
