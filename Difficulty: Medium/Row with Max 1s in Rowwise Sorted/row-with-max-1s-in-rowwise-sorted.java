6ecdc8da000fc3184d6e45ef039b5538963795eb
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-08-24 23:52:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.java
+++ Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.java
@@ -1,19 +1,28 @@
 class Solution {
     public int rowWithMax1s(int[][] mat) {
         // code here
-        int maxOneCount = -1;
-        int maxOneRow = Integer.MIN_VALUE;
+        int maxCount = 0;
+        int index = -1;
         for (int i = 0; i < mat.length; i++) {
-            int oneCount = 0;
-            for (int j = 0; j < mat[i].length; j++) {
-                oneCount += mat[i][j];
-            }
-            if (oneCount > maxOneCount) {
-                maxOneRow = i;
-                maxOneCount = oneCount;
+            int cnt = mat[0].length - binarySearch(mat[i], 1);
+            if (cnt > maxCount) {
+                maxCount = cnt;
+                index = i;
             }
         }
-        if (maxOneCount == 0) return -1;
-        return (maxOneRow );
+        return index;
+    }
+    int binarySearch(int[] arr, int ele) {
+        int ans = arr.length;
+        int low = 0;
+        int high = arr.length-1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (arr[mid] >= ele) {
+                ans = mid;
+                high = mid - 1;
+            } else low = mid + 1;
+        }
+        return ans;
     }
 }
