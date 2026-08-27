7f570df8c107120f19a17ab9a614807a8d931e2a
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-08-27 14:08:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Median in a Row-Wise Sorted Matrix/median-in-a-row-wise-sorted-matrix.java
+++ Difficulty: Medium/Median in a Row-Wise Sorted Matrix/median-in-a-row-wise-sorted-matrix.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int median(int[][] matrix) {
-        // code here
-        int[] arr = new int[matrix.length * matrix[0].length];
-        int i = 0;
-        for (int[] row : matrix) {
-            for (int ele : row) {
-                arr[i] = ele;
-                i++;
-            }
-        }
-        Arrays.sort(arr);
-        return arr[(arr.length - 1) / 2];
-    }
+	public int median(int[][] matrix) {
+		// code here
+		int n = matrix.length;
+		int m = matrix[0].length;
+		int low = Integer.MAX_VALUE;
+		int high = Integer.MIN_VALUE;
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] < low)
+				low = matrix[i][0];
+			if (matrix[i][m - 1] > high)
+				high = matrix[i][m - 1];
+		}
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int lesserNumber = lesserNumber(matrix, mid);
+			if (lesserNumber <= (n * m) / 2) {
+				low = mid + 1;
+			} else high = mid - 1;
+		}
+		return low;
+	}
+	int lesserNumber(int[][] matrix, int mid) {
+		int cnt = 0;
+		for (int[] row : matrix) {
+			for (int ele : row) {
+				if (ele <= mid)
+					cnt++;
+			}
+		}
+		return cnt;
+	}
 }
