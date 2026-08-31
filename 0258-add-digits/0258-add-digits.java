dcd7fef3b610ad128577aa8f5d0fdd12a96482cb
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-08-31 14:01:51 +0530

    Time: 1 ms (98.1%), Space: 42.4 MB (80.39%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,7 +1,20 @@
 class Solution {
     public int addDigits(int num) {
-        if (num == 0) return 0;
-        if (num % 9 == 0) return 9;
-        return num % 9;
+        // if (num == 0) return 0;
+        // if (num % 9 == 0) return 9;
+        // return num % 9;
+
+        return sum(num);
+    }
+
+    int sum(int n) {
+        if (n < 10)
+            return n;
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10);
+            n = n / 10;
+        }
+        return sum(sum);
     }
 }
