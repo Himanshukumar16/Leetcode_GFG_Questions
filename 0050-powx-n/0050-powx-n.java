9e8019f36b0ad0f0142e495225954469d81723ae
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-09-01 20:16:11 +0530

    Time: 0 ms (100%), Space: 47.7 MB (71.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
+        long num = n;
         if (x == 0) return 0;
-        return findPower(x,(long)n);
+        if (n < 0) return findPower(1/x, -num);
+        return findPower(x,n);
     }
     double findPower(double x, long n) {
         double ans = 1;
-        if (n < 0) {
-            x = 1/x;
-            n = -n;
-        }
-        while (n > 0) {
-            if (n%2 != 0) {
-                ans = ans * x;
-                n = n - 1;
-            } else {
-                x = x * x;
-                n = n / 2;
-            }
-        } 
-        return ans;
+        if (n == 0) return 1.0;
+        if (n % 2 != 0) return x * findPower(x,n-1);
+        else return findPower(x*x,n/2);
     }
 }
