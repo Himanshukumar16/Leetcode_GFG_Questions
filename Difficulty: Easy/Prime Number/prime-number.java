abfc6a954acaadaf24a9b7252f2da3954eeab141
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-09-09 06:57:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.java
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -1,15 +1,10 @@
 class Solution {
 	static boolean isPrime(int num) {
 		// code here
-		if (num == 1)
-			return false;
-		return isPrime(num, 2);
-	}
-	static boolean isPrime(int num, int cnt) {
-		if (cnt > Math.sqrt(num))
-			return true;
-		if (num % cnt == 0)
-			return false;
-		return isPrime(num, cnt + 1);
+		if (num == 1) return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+		    if (num % i == 0) return false;
+		}
+		return true;
 	}
 }
