78950f9269d75794892d19da94696bcbaffc9a1e
Himanshu Kumar <himanshu.k.upadhyay05@gmail.com>
2026-09-10 10:30:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.java
+++ Difficulty: Medium/Subset Sums/subset-sums.java
@@ -1,18 +1,16 @@
 class Solution {
-    public ArrayList<Integer> subsetSums(int[] arr) {
-        // code here
-        ArrayList<Integer> lst = new ArrayList<>();
-        subsets(arr,new ArrayList<>(),lst,0,0);
-        return lst;
-    }
-    public void subsets(int[]arr, ArrayList<Integer>lst,ArrayList<Integer>result,int sum, int index) {
-        if (index == arr.length) {
-            result.add(sum);
-            return;
-        }
-        lst.add(arr[index]);
-        subsets(arr,lst,result,sum+arr[index],index+1);
-        lst.removeLast();
-        subsets(arr,lst,result,sum,index+1);
-    }
+	public ArrayList<Integer> subsetSums(int[] arr) {
+		// code here
+		ArrayList<Integer> lst = new ArrayList<>();
+		getSum(arr, lst, 0, 0);
+		return lst;
+	}
+	void getSum(int[] nums, ArrayList<Integer> lst, int index, int sum) {
+		if (index == nums.length) {
+			lst.add(sum);
+			return;
+		}
+		getSum(nums, lst, index + 1, sum + nums[index]);
+		getSum(nums, lst, index + 1, sum);
+	}
 }
